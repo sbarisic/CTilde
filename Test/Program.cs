bdeac629a57dc6504dc13a3825e7571c526cf391
Saša Barišić <sasa.barisic@serengetitech.com>
2024-07-03 13:25:20 +0200

    Model 11

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,13 +7,13 @@ using System.Threading.Tasks;
 using CTilde;
 using CTilde.Langs;
 using CTilde.Expr;
+using Test.Properties;
 
 namespace Test {
 	class Program {
 		static void Main(string[] Args) {
-			Main2();
+			Model11.Main2(Args);
 			return;
-
 
 			if (Args.Length == 0)
 				Args = new string[] { "tests/Test.ct" };
@@ -27,38 +27,6 @@ namespace Test {
 			Lng.Compile(Parser.Parse());
 
 			File.WriteAllText("out.c", Lng.CompileToSource());
-		}
-
-		static void Main2() {
-			while (true) {
-				Console.Write(">> ");
-				string In = Console.ReadLine().Trim();
-
-				int[] Numbers = In.Select(C => int.Parse(C.ToString())).ToArray();
-
-				Console.WriteLine("Numbers: {0}; Checksum: {1}", string.Join("", Numbers.Select(N => N.ToString())), ChecksumMod(Numbers));
-			}
-		}
-
-		static int ChecksumMod(int[] Numbers) {
-			int Last = 10;
-
-			for (int i = 0; i < Numbers.Length; i++) {
-				int N = Numbers[i];
-
-				if (N < 0 || N > 9)
-					throw new Exception("Expected single digit number [0-9]");
-
-				int Temp = (N + Last) % 10;
-
-				if (Temp == 0)
-					Temp = 10;
-
-				Last = (Temp * 2) % 11;
-			}
-
-			int Checksum = 11 - Last;
-			return Checksum;
 		}
 	}
 }
---
+++ Test/Properties/Model11.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Properties {
+	static class Model11 {
+		public static void Main2(string[] Args) {
+			string[] Test = new string[] {
+				"800528864087-23124-1",
+				"800528864079-24015-1",
+				"800528864095-24023-1",
+				"800528864036-24031-1",
+				"800528864079-24040-1",
+				"800528864001-24058-1"
+			};
+
+
+			foreach (string Tst in Test) {
+				string[] P = Tst.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+				string P1 = P[0];
+				string P2 = P[1];
+				string P3 = P[2];
+
+				string P1K = P[0].Last().ToString();
+				string P2K = P[1].Last().ToString();
+
+				P1 = new string(P1.Take(P1.Length - 1).ToArray());
+				P2 = new string(P2.Take(P2.Length - 1).ToArray());
+
+				PrintPnB(P1, P2, P3, P1K, P2K);
+			}
+
+			Console.ReadLine();
+		}
+
+		static void PrintPnB(string P1, string P2, string P3, string P1K, string P2K) {
+			string CalcP1K = KontrolniBroj(P1).ToString();
+			string CalcP2K = KontrolniBroj(P2).ToString();
+
+			Console.Write(P1);
+
+			if (CalcP1K == P1K)
+				Console.ForegroundColor = ConsoleColor.Green;
+			else
+				Console.ForegroundColor = ConsoleColor.Red;
+
+			Console.Write(CalcP1K);
+
+			Console.ResetColor();
+			Console.Write("-{0}", P2);
+
+			if (CalcP2K == P2K)
+				Console.ForegroundColor = ConsoleColor.Green;
+			else
+				Console.ForegroundColor = ConsoleColor.Red;
+
+			Console.Write(CalcP2K);
+
+			Console.ResetColor();
+			Console.WriteLine("-{0}", P3);
+		}
+
+		static int KontrolniBroj(string s) {
+			int Faktor = s.Length + 1;
+			int Sum = 0;
+
+			for (int i = 0; i < s.Length; i++) {
+				int N = int.Parse(s[i].ToString());
+
+				if (N < 0 || N > 9)
+					throw new Exception("Expected single digit number [0-9]");
+
+				Sum += Faktor * N;
+				Faktor--;
+			}
+
+			if (Sum >= 0) {
+				Sum = 11 - (Sum % 11);
+
+				if (Sum > 9)
+					Sum = 0;
+			}
+
+			return Sum;
+		}
+	}
+}

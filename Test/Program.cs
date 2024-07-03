30e386e62d73290c1dbbcc3ffde558e1f6aa85e8
Saša Barišić <sasa.barisic@serengetitech.com>
2024-07-03 14:03:11 +0200

    Removed model 11

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,14 +7,10 @@ using System.Threading.Tasks;
 using CTilde;
 using CTilde.Langs;
 using CTilde.Expr;
-using Test.Properties;
 
 namespace Test {
 	class Program {
 		static void Main(string[] Args) {
-			Model11.Main2(Args);
-			return;
-
 			if (Args.Length == 0)
 				Args = new string[] { "tests/Test.ct" };
 
